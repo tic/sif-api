f0a4a6b1c4834734d72b4b196a3028d757cfd403
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2022-02-28 12:41:26 -0500

    use connection pools in a more intelligent way

--- api_gateway/lib/db.js
+++ api_gateway/lib/db.js
@@ -7,27 +7,34 @@ const { Pool } = require("pg");
 const { parseSsl } = require("pg-ssl");
 
 
+// Create a connection pool to the database
+const pool = new Pool({
+    user: config.TS_USER,
+    host: config.TS_HOST,
+    database: config.TS_DATABASE,
+    password: config.TS_PASSWD,
+    port: config.TS_PORT,
+    ssl: parseSsl()
+});
+
+
 // Executes the query given by @text using SQL
-// parameters provided by @params. Since this
-// code is intended to run in Lambda, connection
-// pools are created dynamically upon a query
-// being submitted to the system.
+// parameters provided by @params. Uses the
+// pooled connection by checking out a client
+// and releasing it after the query is done.
 async function query(text, params) {
-    // Create a connection pool to the database
-    const pool = new Pool({
-        user: config.TS_USER,
-        host: config.TS_HOST,
-        database: config.TS_DATABASE,
-        password: config.TS_PASSWD,
-        port: config.TS_PORT,
-        ssl: parseSsl()
-    });
+    // Check out a client from the pool
+    const client = await pool.connect();
 
-    const dbResponse = await pool.query(text, params);
+    try {
+        // Query the database via the connection pool
+        const dbResponse = await client.query(text, params);
+        return dbResponse;
 
-    await pool.end()
-
-    return dbResponse;
+    } finally {
+        // Release the pool client after the job is done
+        client.release();
+    }
 }
 
 
